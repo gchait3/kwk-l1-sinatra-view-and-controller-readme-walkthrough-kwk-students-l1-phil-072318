74f55bdee804bab126b51cebc6877103532f69b6
Grace T <gtsai64@gmail.com>
2018-08-02 19:07:04 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,12 +1,16 @@
 require_relative 'config/environment'
+require_relative 'models/model.rb'
+
 
 class App < Sinatra::Base
+ 
   get '/' do
-    erb :project
+    erb :home
   end
 
-  # post '/reverse' do
-  #   erb :reversed
-  # end
-
-end
+  post '/' do
+  @my_results = choose_one(params[:Choose])
+  erb :results
+  end
+end 
+  # the @my_results part is how you get results
